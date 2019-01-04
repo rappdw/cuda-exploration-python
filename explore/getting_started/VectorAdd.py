fc1ed67695551a76dca930d2f916044a658698f0
Dan Rapp <drapp@proofpoint.com>
2019-01-04 16:42:13 -0700

    Initial image and code to try out numba cuda support

---
+++ explore/getting_started/VectorAdd.py
@@ -0,0 +1,33 @@
+import numpy as np
+import time
+
+from numba import vectorize, cuda
+
+@vectorize(['float32(float32, float32)'], target='cuda')
+def VectorAdd(a, b):
+    return a + b
+
+def main():
+    A = np.ones(10, dtype=np.float32)
+    B = np.ones(10, dtype=np.float32)
+    start = time.time()
+    C = VectorAdd(A, B)
+    compile_time = time.time() - start
+    print(f"VectorAdd compilation took {compile_time} seconds")
+
+    N = 32000000
+
+    A = np.ones(N, dtype=np.float32)
+    B = np.ones(N, dtype=np.float32)
+
+    start = time.time()
+    C = VectorAdd(A, B)
+    vector_add_time = time.time() - start
+
+    print("C[:5] = " + str(C[:5]))
+    print("C[-5:] = " + str(C[-5:]))
+
+    print(f"VectorAdd took {vector_add_time} seconds")
+
+if __name__=='__main__':
+    main()
